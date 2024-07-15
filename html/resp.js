91affc2d82461e977d7797cce3f51d3d9eccb700
shivagoode <shivam68338@gmail.com>
2024-07-15 18:48:31 +0530

    submit

---
+++ html/resp.js
@@ -0,0 +1,15 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+burger.addEventListener("click",myfunction);
+function myfunction(){
+    navlist.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+
+}
+
+
+
+
+    
+
